3601fd3be42c07e64ec7d97c51bbcb32523049ee
Amr Osama <amrosama2020@gmail.com>
2012-03-09 14:51:57 +0200

    settting up basic watermark controller

---
+++ app/Controller/WatermarkController.php
@@ -0,0 +1,48 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/**
+ * watermark controller.
+ * 
+ * handles the images upload, watermark, and generating the zip file
+ * 
+ * This file will render views from views/watermark/
+ * 
+ * @package       app.Controller
+ */
+ 
+class WatermarkController extends AppController {
+
+	/**
+	 * Controller name
+	 *
+	 * @var string
+	 */
+	public $name = 'Watermark';
+
+	/**
+	 * Default helper
+	 *
+	 * @var array
+	 */
+	public $helpers = array('Html', 'Session');
+
+	/**
+	 * This controller does not use a model
+	 *
+	 * @var array
+	 */
+	public $uses = array();
+
+	/**
+	 * Displays the upload view
+	 *
+	 * @return void
+	 */
+	public function index(){
+		
+		
+	}
+
+}
---
+++ app/View/Watermark/index.ctp
@@ -0,0 +1,1 @@
+watermark stuff here
